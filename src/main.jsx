039ce04509330c2b7389e8037a5deb909835ecb6
Teoslip <tjachiet@gmail.com>
2026-01-16 17:04:15 +0100

    react router tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import App from "./App";
-import Profile from "./Profile";
-import ErrorPage from "./ErrorPage";
-import routes from "./routes";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
